526e2ad90422cc94072249f5d854195f9f539e7b
Jon Smithers <mail@jonsmithers.link>
2018-01-07 18:25:25 -0500

    Put dropbox_api stuff in redux state

--- src/index.js
+++ src/index.js
@@ -8,22 +8,46 @@ import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-ro
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import createHistory from 'history/createBrowserHistory'
 import { Provider } from 'react-redux'
+import Utils from './utils/Utils'
 const history = createHistory()
 const middleware = routerMiddleware(history)
 const store = createStore(
   combineReducers({
-    // ...reducers,
-    router: routerReducer
+    router: routerReducer,
+    dropbox_api: (state, action) => {
+      if (action.type == "hash-change") {
+        let {access_token, token_type, uid, account_id}  = Utils.parseQueryString(store.getState().router.location.hash);
+        return {access_token, token_type, uid, account_id};
+      }
+      return {};
+    },
+    deleteme: (state, action) => {
+      console.log("my redux listener", state, action);
+      return {}
+    },
   }),
   applyMiddleware(middleware)
 )
-console.log(window._store = store);
 
+{ // subscribe to keep dropbox_api in sync
+  let currentHash = undefined;
+  store.subscribe(() => {
+    let previousHash = currentHash;
+    currentHash = store.getState().router && store.getState().router.location.hash;
+    if (currentHash === previousHash) {
+      return;
+    }
+    store.dispatch({type: "hash-change", currentHash})
+  });
+}
+
+// for debugging
+console.log('_store', window._store = store);
 
 ReactDOM.render(
   <Provider store={store}>
       <ConnectedRouter history={history}>
-          <App /> 
+          <App />
       </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
