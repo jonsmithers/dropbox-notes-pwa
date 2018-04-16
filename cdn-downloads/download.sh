ba1cb1cedb288e6f78bb3a59098a6b0de94c122d
Jon Smithers <mail@jonsmithers.link>
2018-04-15 21:52:19 -0400

    Initial build

---
+++ cdn-downloads/download.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+wget https://cdnjs.cloudflare.com/ajax/libs/redux/3.7.2/redux.js
+wget https://unpkg.com/@reactivex/rxjs@5.0.0-beta.12/dist/global/Rx.js
+wget https://cdnjs.cloudflare.com/ajax/libs/dropbox.js/4.0.3/Dropbox-sdk.js
+# wget https://unpkg.com/dropbox/dist/Dropbox-sdk.min.js"
