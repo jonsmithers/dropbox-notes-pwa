df2bfed494136f1b760b9e21d03aadb7ae2c3d0c
Jon Smithers <mail@jonsmithers.link>
2018-04-20 23:33:04 -0400

    Start working on Dao apis

--- state-persister.js
+++ state-persister.js
@@ -1,5 +1,78 @@
 import objectPath from './lib/object-path.js';
 import {store, DropboxDispatchers, DropboxCacheDispatchers} from "./app-store.js";
+import {Dropbox} from '../lib/dropbox.js';
+
+class DaoInterface {
+  constructor() {
+    let keys = Object.getOwnPropertyNames(DaoInterface.prototype);
+    let unimplementedMethods = keys.filter(key => key !== 'constructor').filter(key => this[key] === DaoInterface.prototype[key]);
+    if (unimplementedMethods.length) {
+      console.warn('unimplemented methods', unimplementedMethods);
+      throw new Error("DaoInterface not been properly implemented");
+    }
+  }
+  /**
+   * @param {string} path
+   */
+  getLastUpdated() {}
+  /**
+   */
+  list() {}
+  /**
+   * @param {string} path
+   */
+  read() {}
+  update() {}
+  create() {}
+}
+window.DaoInterface = DaoInterface;
+
+class LocalStorageDao extends DaoInterface {
+  constructor() { super(); }
+}
+window.LocalStorageDao = LocalStorageDao;
+
+class DropboxDao extends DaoInterface {
+  constructor(accessToken) {
+    super();
+    // this._dropbox = new Dropbox({ accessToken: store.getState().dropbox.access_token});
+    this._dropbox = new Dropbox({accessToken});
+  }
+  async list(path='') {
+    let response = await this._dropbox.filesListFolder({
+      path, 
+      recursive: false, 
+      include_media_info: false, 
+      include_deleted: false, 
+      include_has_explicit_shared_members: false, 
+      include_mounted_folders: false});
+
+    return response.entries.map((entry) => {
+      return {
+        lastUpdated: Date.parse(entry.server_modified),
+        path: entry.path_lower,
+        name: entry.name,
+        _extras: entry,
+      };
+    });
+  }
+  async read(path) {
+    let {fileBlob} = await this._dropbox.filesDownload({path});
+    let fileReader = new FileReader();
+    let readPromise = new Promise(resolve => fileReader.onload=resolve);
+    fileReader.readAsText(fileBlob);
+    await readPromise;
+    return fileReader.result;
+  }
+  update() {}
+  create() {}
+  getLastUpdated() {}
+  static instance() {
+    return new DropboxDao(store.getState().dropbox.access_token);
+  }
+}
+window.DropboxDao = DropboxDao;
+
 
 if (!store.getState().dropbox.access_token && localStorage.getItem('dropbox-authentication')) {
   DropboxDispatchers.authenticate(JSON.parse(localStorage.getItem('dropbox-authentication')));
