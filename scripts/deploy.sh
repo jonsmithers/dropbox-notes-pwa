eb01c7a91adabef17ecbd201311d014745604d30
Jon Smithers <mail@jonsmithers.link>
2018-04-21 12:50:23 -0400

    Automate more deploy steps

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,3 +6,11 @@ cp dist-index.html ./dist/index.html
 mkdir ./dist/cdn-downloads/
 cp -r ./cdn-downloads/ ./dist/
 cp --parents node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js ./dist/
+
+sha=`git show-ref HEAD -s`
+cd ./dist
+git add --all
+git commit -m "Deploy $sha"
+
+echo "TO DEPLOY:"
+echo "cd dist/; git push"
