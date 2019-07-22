018803e83e37b15ddc46fab033d6f8049531de27
Jon Smithers <mail@jonsmithers.link>
2019-07-22 12:41:06 -0400

    Switch to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Dropbox } from 'dropbox'
+import { Dropbox, files as filesTypes } from 'dropbox'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { createContainer } from "unstated-next"
 import Button from '@material-ui/core/Button';
 
-function useDropboxCredentials(initialState = null) {
+type DropboxCredentialsType = {[index:string]:string};
+function useDropboxCredentials(initialState: DropboxCredentialsType|null = null) {
   let [credentials, setCredentials] = useState(initialState)
   return { credentials, setCredentials }
 }
@@ -28,17 +29,17 @@ function App() {
             <img src={logo} className="App-logo" alt="logo" />
             <p>
                 Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              > Learn React </a>
-            </header>
-          </div>
-        </Router>
-    </DropboxCredentials.Provider>
+            </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          > Learn React </a>
+    </header>
+  </div>
+    </Router>
+  </DropboxCredentials.Provider>
   );
 }
 
@@ -56,10 +57,13 @@ function DropboxLoginPopup() {
     const location = window.location;
     if (location.hash) {
       const params = new URLSearchParams(location.hash.slice(1));
-      const paramsObject = Array.from(params.entries()).reduce((accumulator, [k,v]) => {
+
+      type KeyValPair = [string, string]
+      const paramsObject: DropboxCredentialsType = Array.from(params.entries()).reduce((accumulator: DropboxCredentialsType, [k, v]: KeyValPair) => {
         accumulator[k] = v;
         return accumulator;
       }, {});
+
       for (let [key, val] of params.entries()) {
         sessionStorage.setItem(`${dropbox_credential_prefix}${key}`, val);
       }
@@ -70,11 +74,11 @@ function DropboxLoginPopup() {
   useEffect(() => {
     // get creds from session stroage
     if (!dropboxCredentials.credentials && sessionStorage.getItem(`${dropbox_credential_prefix}access_token`)) {
-      const credentials = {};
+      const credentials: DropboxCredentialsType = {};
       for (let i = 0; i < sessionStorage.length; i++) {
-        const key = sessionStorage.key(i);
+        const key: string = sessionStorage.key(i)!;
         if (key.startsWith(dropbox_credential_prefix)) {
-          credentials[key.replace(dropbox_credential_prefix, '')] = sessionStorage.getItem(key);
+          credentials[key.replace(dropbox_credential_prefix, '')] = sessionStorage.getItem(key)!;
         }
       }
       dropboxCredentials.setCredentials(credentials);
@@ -96,13 +100,15 @@ function DropboxLoginPopup() {
 
 function Home() {
   const dropboxCredentials = DropboxCredentials.useContainer();
-  const [files, setFiles] = useState(null);
+  const [files, setFiles] = useState<filesTypes.ListFolderResult|null>(null);
   useEffect(() => {
     console.log('files', files);
     console.log('credentials', dropboxCredentials.credentials);
     if (!files && dropboxCredentials.credentials) {
       const dropbox = new Dropbox({accessToken: dropboxCredentials.credentials.access_token, fetch: window.fetch})
-      dropbox.filesListFolder({path: ''}).then(setFiles)
+      dropbox.filesListFolder({path: ''}).then(x => {
+        setFiles(x);
+      });
     }
   });
   return (
