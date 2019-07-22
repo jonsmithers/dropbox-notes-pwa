7d624deae6713cddc96dd3dd1e8cfc0e31e21848
Jon Smithers <mail@jonsmithers.link>
2019-07-22 08:48:26 -0400

    Install material-ui, hydrate credentials from session storage, reorganize

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import './App.css';
 import { Dropbox } from 'dropbox'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { createContainer } from "unstated-next"
+import Button from '@material-ui/core/Button';
 
 function useDropboxCredentials(initialState = null) {
   let [credentials, setCredentials] = useState(initialState)
   return { credentials, setCredentials }
 }
 const DropboxCredentials = createContainer(useDropboxCredentials)
+const dropbox_credential_prefix = 'dropbox-credentials.';
 
-
-const dropbox = new Dropbox({ clientId: "wl4k2y0xsplg260"  });
+const dropbox = new Dropbox({ clientId: "wl4k2y0xsplg260", fetch: window.fetch });
 const authUrl = dropbox.getAuthenticationUrl(window.location.toString())
 
 function App() {
   return (
     <DropboxCredentials.Provider>
+      <DropboxLoginPopup />
       <Router>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About} />
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
@@ -33,8 +37,6 @@ function App() {
               > Learn React </a>
             </header>
           </div>
-          <Route exact path="/" component={Home}/>
-          <Route path="/about" component={About} />
         </Router>
     </DropboxCredentials.Provider>
   );
@@ -47,30 +49,55 @@ function About() {
   )
 }
 
-function Home() {
+function DropboxLoginPopup() {
   const dropboxCredentials = DropboxCredentials.useContainer();
   useEffect(() => {
+    // get creds from dropbox return url
     const location = window.location;
-    console.log('dropboxCredentials.credentials', dropboxCredentials.credentials);
     if (location.hash) {
       const params = new URLSearchParams(location.hash.slice(1));
-      dropboxCredentials.setCredentials(Array.from(params.entries()).reduce((accumulator, [k,v]) => {
+      const paramsObject = Array.from(params.entries()).reduce((accumulator, [k,v]) => {
         accumulator[k] = v;
         return accumulator;
-      }, {}));
+      }, {});
+      for (let [key, val] of params.entries()) {
+        sessionStorage.setItem(`${dropbox_credential_prefix}${key}`, val);
+      }
+      dropboxCredentials.setCredentials(paramsObject);
       location.hash = '';
-      // location.href = location.href.replace(/#.*$/, '')
     }
   });
+  useEffect(() => {
+    // get creds from session stroage
+    if (!dropboxCredentials.credentials && sessionStorage.getItem(`${dropbox_credential_prefix}access_token`)) {
+      const credentials = {};
+      for (let i = 0; i < sessionStorage.length; i++) {
+        const key = sessionStorage.key(i);
+        if (key.startsWith(dropbox_credential_prefix)) {
+          credentials[key.replace(dropbox_credential_prefix, '')] = sessionStorage.getItem(key);
+        }
+      }
+      dropboxCredentials.setCredentials(credentials);
+      console.log('credentials', credentials);
+    }
+  })
+  if (dropboxCredentials.credentials) {
+    return <></>
+  }
+  return (
+    <a
+      className="App-link"
+      href={authUrl}
+    >
+      <Button color="primary" variant="contained">Authenticate Dropbox</Button>
+    </a>
+  );
+}
+
+function Home() {
   return (
     <div>
       <span>Im home</span>
-        {dropboxCredentials.credentials ? <span>dropbox logged in </span> :
-            <a
-              className="App-link"
-              href={authUrl}
-            >dropbox</a>
-        }
     </div>
   );
 }
