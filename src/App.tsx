ec76d175ddd9b74bf34db341a5941b31539a4ea0
Jon Smithers <mail@jonsmithers.link>
2019-07-28 17:21:47 -0400

    Add svg to dropbox link

--- src/App.tsx
+++ src/App.tsx
@@ -99,11 +99,12 @@ function DropboxLoginPopup() {
     return <></>
   }
   return (
-    <a
-      className="App-link"
-      href={authUrl}
-    >
-      <Button color="primary" variant="contained">Authenticate Dropbox</Button>
+    <a href={authUrl} style={{textDecoration: 'none'}}>
+      <Button variant="contained">
+        <svg xmlns="http://www.w3.org/2000/svg" role="img" width="32px" height="32px" viewBox="0 0 32 32" style={{fill:'#0062ff'}} data-reactid="12"><title data-reactid="13"></title><path d="M8 2.4l8 5.1-8 5.1-8-5.1 8-5.1zm16 0l8 5.1-8 5.1-8-5.1 8-5.1zM0 17.7l8-5.1 8 5.1-8 5.1-8-5.1zm24-5.1l8 5.1-8 5.1-8-5.1 8-5.1zM8 24.5l8-5.1 8 5.1-8 5.1-8-5.1z" data-reactid="14"></path></svg>
+          &nbsp;
+          Authenticate Dropbox
+      </Button>
     </a>
   );
 }
